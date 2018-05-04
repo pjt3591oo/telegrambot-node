39b3c41ebbb42506f76fe8c0ba365f6119d3dae4
박정태 <pjt@bagjeongtaeui-iMac.local>
2018-05-04 11:29:09 +0900

    한글포함 유무 검사 로직 수정한다

--- utils/bots/telegram/makeMessage.js
+++ utils/bots/telegram/makeMessage.js
@@ -1,10 +1,10 @@
 const {fromKoToEn, fromEnToKo} = require('../../../utils/translate')
-const {isChkKrtext} = require('./strCheck')
+const {isIncludeKrtext} = require('./strCheck')
 
 async function makeMessage(originMsg){
     let translateEnMsg, translateKoMsg, message;
 
-    if (isChkKrtext(originMsg)){ // ko -> en
+    if (isIncludeKrtext(originMsg)){ // ko -> en
         translateEnMsg = await fromKoToEn(originMsg);
         message = `원문(original): ${originMsg}\n영문(English): ${translateEnMsg}`
     } else {  // en -> ko
--- utils/bots/telegram/strCheck.js
+++ utils/bots/telegram/strCheck.js
@@ -1,12 +1,15 @@
-var isChkKrtext = function(text) {
-   const regexp = /^[ㄱ-ㅎ|가-힣|\*]+$/
+var isIncludeKrtext = function(text) {
+   var regexp =/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*/gi;
+   var text2 = text.match(regexp);
 
-   const text2 = regexp.test(text);
-   if (text2) return true;
-
-   return false;
+   if (text2 != null) {
+      for (i=0; i < text2.length ; i++) {
+         if(text2[i]) return true // 한글 포함되어 있으면 true 반환
+      }
+   }
+   return false
 }
 
 module.exports = {
-    isChkKrtext:isChkKrtext
+    isIncludeKrtext: isIncludeKrtext
 }
